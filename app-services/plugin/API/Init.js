b4e920e97a864593eeb1b4c238dfaad1e58fe5ba
ozaka001 <108465466+ozaka001@users.noreply.github.com>
2024-09-02 12:17:33 +0700

    transfer page

--- app-services/plugin/API/Init.js
+++ app-services/plugin/API/Init.js
@@ -1,12 +1,10 @@
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
-// console.log(process.env.BASEURL)
+// console.log(process.env.BASEURL);
 // "http://localhost:3005"
 const Axios = axios.create({
   baseURL:
-    process.env.BASEURL ?? "https://api-itcmcoin-dev.product-services.com",
+    process.env.BASEURL || "https://api-itcmcoin-dev.product-services.com",
 });
 Axios.defaults.timeout = 2500;
 
